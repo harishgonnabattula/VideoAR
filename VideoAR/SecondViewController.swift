bec7a196890aa248fcfde5f99646d8275165be5e
Ninja <hawk3134@gmail.com>
2018-11-02 17:56:56 -0700

    Initial Commit

---
+++ VideoAR/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  VideoAR
+//
+//  Created by Ninja on 11/2/18.
+//  Copyright © 2018 Ninja. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
